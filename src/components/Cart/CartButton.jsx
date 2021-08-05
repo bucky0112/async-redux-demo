3a9456b653b6aa110863faa40f2f5602816d0b77
Bucky <bucky0112@gmail.com>
2021-08-05 14:51:46 +0800

    handle show cart and mycart quantity

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import classes from './CartButton.module.css'
+import { uiActions } from './../../store/uiSlice'
 
 const CartButton = (props) => {
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity)
+
+  const dispatch = useDispatch()
+
+  const handleToggle = () => {
+    dispatch(uiActions.toggle())
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={handleToggle}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalQuantity}</span>
     </button>
   )
 }
